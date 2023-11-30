0cc3bb5d91e492c8347add2a354a83577d847980
Santu Jana <115920400+akku27-cse@users.noreply.github.com>
2023-11-30 11:08:33 +0530

    Update BubbleSort.java

--- Nov-code-practice/Java40Questions/Array/BubbleSort.java
+++ Nov-code-practice/Java40Questions/Array/BubbleSort.java
@@ -17,5 +17,9 @@ public class BubbleSort {
         System.out.println("After sorting array");
         for (i = 0; i < arr.length ;i++)
         System.out.println(arr[i]);
+        System.out.println("reverse sorring");
+        for(i=arr.length-1;i>=0;i--)
+            System.out.println(arr[i]);
+                           
     }
 }
